847ce7f5f6a7df0e178b9013c596eba86803b52b
nitikagoel01 <nitikagoel.1693@gmail.com>
2017-06-19 08:02:31 +0000

    Done

--- build.py
+++ build.py
@@ -2,9 +2,7 @@ import operator
 
 
 def solution_asc(dic):
-    return sorted(dic.items(), key=operator.itemgetter(0))
-
-
+    return sorted(dic.items())
 
 def solution_desc(dic):
     return sorted(dic.items(),key=operator.itemgetter(0),reverse=True)
